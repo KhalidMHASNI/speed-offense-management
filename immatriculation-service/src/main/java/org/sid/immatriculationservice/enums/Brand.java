ff8a70c0a530506d919cb87b091397b7e47b7a69
Gamareel <kmhasni15@gmail.com>
2023-06-26 12:21:21 +0100

    pushing

---
+++ immatriculation-service/src/main/java/org/sid/immatriculationservice/enums/Brand.java
@@ -0,0 +1,5 @@
+package org.sid.immatriculationservice.enums;
+
+public enum Brand {
+    MERCEDES,BMW,RENAULT,DACIA,VOLVO
+}
---
+++ radar-service/src/main/java/org/sid/radarservice/enums/Brand.java
@@ -0,0 +1,5 @@
+package org.sid.radarservice.enums;
+
+public enum Brand {
+    MERCEDES,BMW,RENAULT,DACIA,VOLVO
+}
