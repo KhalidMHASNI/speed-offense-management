10497b9cd34629573900b8bfd29a2a4ec491b8cf
Gamareel <kmhasni15@gmail.com>
2023-07-01 01:37:15 +0100

    mm

--- radar-web-app/src/app/app.module.ts
+++ radar-web-app/src/app/app.module.ts
@@ -1,17 +1,32 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VehiclesComponent } from './vehicles/vehicles.component';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { InfractionsComponent } from './infractions/infractions.component';
 import { InfractiondetailComponent } from './infractiondetail/infractiondetail.component';
 import { VehicledetailComponent } from './vehicledetail/vehicledetail.component';
 import { RadarComponent } from './radar/radar.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RadardetailComponent } from './radardetail/radardetail.component';
 import { HomeComponent } from './home/home.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080/auth',
+        realm: 'radarmax-realm',
+        clientId: 'radarmax-client',
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        flow: 'standard',
+      },
+    });
+}
 
 @NgModule({
   declarations: [
@@ -22,16 +37,24 @@ import { HomeComponent } from './home/home.component';
     VehicledetailComponent,
     RadarComponent,
     RadardetailComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    KeycloakAngularModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
